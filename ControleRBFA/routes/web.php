4a28a9335b8ecb4be31f8123a9cc4ee3fc80e6d4
IgorCremonezi <igc2009_guilherme@hotmail.com>
2024-10-09 17:40:47 -0300

    rotas

--- ControleRBFA/routes/web.php
+++ ControleRBFA/routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\ObrigacaoController;
+use App\Http\Controllers\RotinaController;
+use App\Http\Controllers\SubrotinaController;
+use App\Http\Controllers\TarefaController;
+use App\Http\Controllers\ControleObrigacaoController;
+use App\Http\Controllers\ControleTarefaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +24,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('departamentos', DepartamentoController::class);
+    Route::resource('funcionarios', FuncionarioController::class);
+    Route::resource('empresas', EmpresaController::class);
+    Route::resource('obrigacoes', ObrigacaoController::class);
+    Route::resource('rotinas', RotinaController::class);
+    Route::resource('subrotinas', SubrotinaController::class);
+    Route::resource('tarefas', TarefaController::class);
+    Route::resource('controles_obrigacoes', ControleObrigacaoController::class);
+    Route::resource('controles_tarefas', ControleTarefaController::class);
 });
 
 require __DIR__.'/auth.php';
